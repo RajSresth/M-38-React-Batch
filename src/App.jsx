d48c3f9f0cc0c27c3a111d91f80f9d221183090d
Shresth Rajput <rajshresth2@gmail.com>
2026-01-28 11:22:11 +0530

    Default and Render Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import User from "./Topic-3 map reduce filter/User.jsx";
+import User from "./Topic-3 map reduce filter/User";
+import Parent from "./Topic-5 default and render props/Parent";
 
 const App = () => {
     return <div id="app-component">
-                <User/>
+                <Parent/>
             </div>
 }
 
