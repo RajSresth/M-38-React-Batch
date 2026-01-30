1ae6067898d6b9b106394e7ec996f676f5afb054
Shresth Rajput <rajshresth2@gmail.com>
2026-01-30 11:31:12 +0530

    React State Variable

--- src/Topic-7 useState Hook/Counter.jsx
+++ src/Topic-7 useState Hook/Counter.jsx
@@ -1,37 +1,21 @@
-import React,{useState} from "react"
+import { useState } from "react";
 
 const Counter = () => {
+  // React state variable
+  const [count, setCount] = useState(0);
 
-    // React state variable
-    const [countReact,setCountReact] = useState(0);  
+  console.log("counter render");
 
-    console.log("counter render");
-
-    // Js local variable inside component
-    let countJs = 0; 
-    const incrementCountJs = ()=>{
-        countJs = countJs + 1;
-        console.log("countJs:",countJs);
-    }
-
-    const incrementCountReact=()=>{
-            setCountReact(countReact + 1);
-    }
-    return <div>
-        <h1>Counter Component</h1>
-        <h2>Count Js:{countJs}</h2>
-        <h2>Count React:{countReact}</h2>
-        <button onClick={incrementCountJs}>Incr Js Count</button>
-        <button onClick={incrementCountReact}>Incr React Count</button>
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>Counter Component</h1>
+      <h2>Count:{count}</h2>
+      <button onClick={increment}>Increment</button>
     </div>
-}
+  );
+};
 
 export default Counter;
-
-
-
-
-
-
-
- 
