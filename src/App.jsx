eaa37d6bbd9ce67938bbea738b291a51e9bfafbe
Shresth Rajput <rajshresth2@gmail.com>
2026-02-19 10:41:57 +0530

    Controlled and Uncontrolled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Parent from "./Topic-16 useCallback/Parent";
+// import Controlled from "./Topic-17 Controlled and Uncontrolled/Controlled";
+import Uncontrolled from "./Topic-17 Controlled and Uncontrolled/Uncontrolled"
 
 const App = () => {
   return (
     <div id="app-component">
-     <Parent/>
+      {/* <Controlled/> */}
+      <Uncontrolled/>
     </div>
   )
 }
